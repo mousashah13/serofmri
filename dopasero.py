14ed83c1dc0d9d7654fbaa067e6b7ce31aedc4eb
mousashah13 <124420755+mousashah13@users.noreply.github.com>
2023-02-06 18:00:20 +0500

    Update dopasero.py

--- dopasero.py
+++ dopasero.py
@@ -1,44 +1,34 @@
 import numpy as np
 import nibabel as nib
-from nilearn import image
-from sklearn.linear_model import LogisticRegression
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
 
-
+# Load fMRI data
 img = nib.load('preprocessed_fmri_data.nii')
 data = img.get_fdata()
 
+# Flatten the data to a 2D array
+data = np.reshape(data, (np.prod(data.shape[:3]), data.shape[3]))
 
-design_matrix = np.zeros((data.shape[3], 2))
-design_matrix[:, 0] = 1
-design_matrix[:, 1] = np.arange(data.shape[3])
-
-estimated_flows = np.zeros(data.shape[:3])
-for i in range(data.shape[0]):
-    for j in range(data.shape[1]):
-        for k in range(data.shape[2]):
-            voxel_data = data[i, j, k, :]
-            model = LinearRegression(fit_intercept=False).fit(design_matrix, voxel_data)
-            estimated_flows[i, j, k] = model.coef_[1]
-
-new_img = nib.Nifti1Image(estimated_flows, img.affine)
-nib.save(new_img, 'estimated_flows.nii')
-
-img = nib.load('estimated_flows.nii')
-data = img.get_fdata()
-data = np.reshape(data, (np.prod(data.shape[:3]), 1))
-
+# Labels for neurotransmitter expression
 labels = np.zeros(data.shape[0])
-labels[data < 0] = 0 # low values correspond to serotonin
-labels[data >= 0] = 1 # high values correspond to dopamine
+labels[np.mean(data, axis=1) < 0] = 0 # low values correspond to serotonin
+labels[np.mean(data, axis=1) >= 0] = 1 # high values correspond to dopamine
 
-
+# Split the data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(data, labels, test_size=0.2, random_state=0)
 
-classifier = LogisticRegression(solver='lbfgs').fit(X_train, y_train)
+# Train a SVM classifier
+classifier = SVC(kernel='linear').fit(X_train, y_train)
 
+# Predict neurotransmitter expression using the test set
 y_pred = classifier.predict(X_test)
 
+# Calculate the accuracy of the classifier
 acc = accuracy_score(y_test, y_pred)
 print('Accuracy: {:.2f}%'.format(acc * 100))
+
+# Evaluate the classifier using cross-validation
+scores = cross_val_score(classifier, data, labels, cv=5)
+print("Cross-Validation Accuracy: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
